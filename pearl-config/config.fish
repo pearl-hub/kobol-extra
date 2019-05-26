3b24d4c541a0a7edf428a39dac402bee0304fb87
Filippo Squillace <squillace.filippo@gmail.com>
2019-05-26 16:38:33 +1000

    Fix config.fish

--- pearl-config/config.fish
+++ pearl-config/config.fish
@@ -1,6 +1,8 @@
 
-switch $KOBOL_MANPATH
+
+switch "$KOBOL_MANPATH"
 case "*$PEARL_PKGDIR/mans*"
+    # The path already exists. No action required
     echo > /dev/null
 case '*'
     set -x KOBOL_MANPATH "$PEARL_PKGDIR/mans:$KOBOL_MANPATH"
